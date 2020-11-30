ca02a4776d8c7b42bf2ed46ae9dc6ad31fb6fcbc
kandashi <jacobmpalmer@live.co.uk>
2020-11-30 16:14:22 +0000

    Add files via upload

--- Spell Macros/alter self.js
+++ Spell Macros/alter self.js
@@ -1,11 +1,9 @@
 let item = actor.items.find(i => i.name === `Unarmed Strike`);
-//macro calls for setting flag data 
 let ActorSetFlag = game.macros.getName("ActorSetFlag");
 let ActorUnSetFlag = game.macros.getName("ActorUnSetFlag");
 
 if (args[0] === "on") {
     if (token) {
-        //query for which section of alter self to use
         new Dialog({
             title: "Are you using Natural Weapons",
             content: "<p>Yes or no</p>",
@@ -17,10 +15,9 @@ if (args[0] === "on") {
                             ChatMessage.create({content: "No unarmed strike found"})
                             return;
                         }
-                        // update the "unarmed strike" of the actor to change to 1d6 + mod 
                         let copy_item = duplicate(item);
-                        ActorSetFlag.execute(args[1],'world', 'AlterSelfSpell', item.data.data.damage.parts[0][0])
-                        copy_item.data.data.damage.parts[0][0] = "1d6 +@mod"
+                        ActorSetFlag.execute(args[1],'world', 'AlterSelfSpell', copy_item.data.damage.parts[0][0])
+                        copy_item.data.damage.parts[0][0] = "1d6 +@mod"
                         actor.updateEmbeddedEntity("OwnedItem", copy_item);
                         ChatMessage.create({content: "Unarmed strike is altered"})
                     }
@@ -33,11 +30,11 @@ if (args[0] === "on") {
         }).render(true);
     }
 } 
-// update the "unarmed strike" of the actor to revert to original data
 if(args[0] === "off") {
-    let damage = target.actor.getFlag('world', 'Alter self')
+    let damage = actor.getFlag('world', 'AlterSelfSpell')
+debugger
     let copy_item = duplicate(item);
-    copy_item.data.data.damage.parts[0][0] = damage
+    copy_item.data.damage.parts[0][0] = damage
     actor.updateEmbeddedEntity("OwnedItem", copy_item);
     ActorUnSetFlag.execute(args[1],'world', 'AlterSelfSpell',);
     ChatMessage.create({ content: `Alter Self expired, unarmed strike returned` })

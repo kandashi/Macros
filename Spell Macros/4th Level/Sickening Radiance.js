7068fbb4f2746baff3edace34ec545c761e32938
kandashi <jacobmpalmer@live.co.uk>
2021-02-25 20:35:29 +0000

    Update Sickening Radiance.js

--- Spell Macros/4th Level/Sickening Radiance.js
+++ Spell Macros/4th Level/Sickening Radiance.js
@@ -1,4 +1,4 @@
-//DAE Item Macro  @attributes.spell.dc
+//DAE Item Macro  @attributes.spelldc
 const lastArg = args[args.length - 1];
 let tactor;
 if (lastArg.tokenId) tactor = canvas.tokens.get(lastArg.tokenId).actor;
